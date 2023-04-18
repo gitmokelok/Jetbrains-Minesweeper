be2925f17a7b51967bf932ea778fbd006c9433ef
DmitrijMamajev <13055714+gitmokelok@users.noreply.github.com>
2023-04-18 10:48:57 +0200

    Initial commit

---
+++ Minesweeper (Java)/task/src/minesweeper/Main.java
@@ -0,0 +1,14 @@
+package minesweeper;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+
+        for (int i = 0; i< 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(".");
+            }
+            System.out.println();
+        }
+    }
+}
